155a871be35e70c7092ba40de12bb2b2ab28f026
MatheusTostes <matheus_tostes@hotmail.com>
2023-06-01 06:54:04 -0300

    refactor(user): set try catches on controller

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -3,34 +3,63 @@ import ListUsers from '../services/UserServices/ListUsers';
 import UpdateService from '../services/UserServices/UpdateService';
 import CreateService from '../services/UserServices/CreateService';
 import ShowService from '../services/UserServices/ShowService';
+import DeleteService from '../services/UserServices/DeleteService';
 
 export const createService = async (req: Request, res: Response) => {
   const { body } = req;
 
-  const user = await CreateService(body);
+  try {
+    const user = await CreateService(body);
 
-  return res.status(201).json(user);
+    return res.status(201).json(user);
+  } catch (err) {
+    return res.status(400).json({ message: (err as Error).message });
+  }
 };
 
 export const updateService = async (req: Request, res: Response) => {
   const { body } = req;
   const { id } = req.params;
 
-  const user = await UpdateService({ id, ...body });
+  try {
+    const user = await UpdateService({ id, ...body });
 
-  return res.status(201).json(user);
+    return res.status(201).json(user);
+  } catch (err) {
+    return res.status(400).json({ message: (err as Error).message });
+  }
 };
 
 export const listUsers = async (req: Request, res: Response) => {
-  const users = await ListUsers();
+  try {
+    const users = await ListUsers();
 
-  return res.status(200).json(users);
+    return res.status(200).json(users);
+  } catch (err) {
+    return res.status(400).json({ message: (err as Error).message });
+  }
 };
 
 export const showUser = async (req: Request, res: Response) => {
   const { id } = req.params;
 
-  const user = await ShowService(id);
+  try {
+    const user = await ShowService(id);
 
-  return res.status(200).json(user);
+    return res.status(200).json(user);
+  } catch (err) {
+    return res.status(400).json({ message: (err as Error).message });
+  }
+};
+
+export const deleteUser = async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  try {
+    await DeleteService(id);
+
+    return res.status(200).json({ message: 'User deleted' });
+  } catch (err) {
+    return res.status(400).json({ message: (err as Error).message });
+  }
 };
