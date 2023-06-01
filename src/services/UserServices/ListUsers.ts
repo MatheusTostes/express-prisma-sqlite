baeee4e3f2edd8848c3b99c615e595dc227064ef
MatheusTostes <matheus_tostes@hotmail.com>
2023-06-01 07:09:40 -0300

    fix(users): list tickets give only name and email params

--- src/services/UserServices/ListUsers.ts
+++ src/services/UserServices/ListUsers.ts
@@ -3,24 +3,15 @@ import prisma from '../../utils/prisma';
 // create a type based on prisma User but without password
 
 interface IUser {
-  id: number;
   email: string;
   name: string;
-  login: string;
-  createdAt: Date;
-  updatedAt: Date;
 }
 
 const ListUsers = async (): Promise<IUser[]> => {
   const users = await prisma.user.findMany({
-    // exclude password
     select: {
-      id: true,
       name: true,
       email: true,
-      login: true,
-      createdAt: true,
-      updatedAt: true,
     },
   });
 
