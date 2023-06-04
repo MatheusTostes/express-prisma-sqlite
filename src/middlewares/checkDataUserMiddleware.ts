563423f169a7c91e60791976283fb45be2eec1c1
MatheusTostes <matheus_tostes@hotmail.com>
2023-06-04 20:19:43 -0300

    fix(users): on update detect if login or email is from same user

--- src/middlewares/checkDataUserMiddleware.ts
+++ src/middlewares/checkDataUserMiddleware.ts
@@ -1,19 +1,25 @@
 import { NextFunction, Request, Response } from 'express';
 import prisma from '../utils/prisma';
 
-const checkEmailInUse = async (emailToCheck: string) => {
-  const emailInUse = await prisma.user.findUnique({
+const checkEmailInUse = async (emailToCheck: string, id: number) => {
+  const emailInUse = await prisma.user.findFirst({
     where: {
       email: emailToCheck,
+      NOT: {
+        id,
+      },
     },
   });
   return emailInUse;
 };
 
-const checkLoginInUse = async (loginToCheck: string) => {
-  const loginInUse = await prisma.user.findUnique({
+const checkLoginInUse = async (loginToCheck: string, id: number) => {
+  const loginInUse = await prisma.user.findFirst({
     where: {
       login: loginToCheck,
+      NOT: {
+        id,
+      },
     },
   });
   return loginInUse;
@@ -26,6 +32,7 @@ const checkDataUserMiddleware = async (
   res: Response,
   next: NextFunction,
 ): Promise<void | Response> => {
+  const id = Number(req.params.id);
   const { name, email, password, login } = req.body;
 
   let message;
@@ -34,7 +41,7 @@ const checkDataUserMiddleware = async (
     message = 'NAME_REQUIRED';
   } else if (!email) {
     message = 'EMAIL_REQUIRED';
-  } else if (await checkEmailInUse(email)) {
+  } else if (await checkEmailInUse(email, id)) {
     message = 'EMAIL_ALREADY_IN_USE';
   } else if (!email.match(emailRegex)) {
     message = 'INVALID_EMAIL';
@@ -42,7 +49,7 @@ const checkDataUserMiddleware = async (
     message = 'PASSWORD_MUST_CONTAIN_AT_LEAST_6_CHARACTERS';
   } else if (!login) {
     message = 'LOGIN_REQUIRED';
-  } else if (await checkLoginInUse(login)) {
+  } else if (await checkLoginInUse(login, id)) {
     message = 'LOGIN_ALREADY_IN_USE';
   }
 
